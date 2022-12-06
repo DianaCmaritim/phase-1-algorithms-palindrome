139d27ad39d37be6d67f7d1365fdff0f681a050b
Diana <maritimdiane96@gmail.com>
2022-12-06 19:42:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-function isPalindrome(word) {
+function isPalindrome(string) {
   // Write your algorithm here
-}
 
-/* 
-  Add your pseudocode here
+  const length = string.length;
+
+  for (let i = 0; i < length / 2; i++){
+
+  if (string[i] !== string[string.length-1-i]){
+    return false;
+  }
+  }return true;
+  }
+    console.log("abba")
+
+
+
+  //Add your pseudocode here
+/*find the length of the string
+
+iterate through half of the string using for loop
+
+use the if condition if the first half and
+last half of the string characters are the same
+
+return false if string does meet  condition
+else return true
 */
 
-/*
-  Add written explanation of your solution here
+ //Add written explanation of your solution here
+/*The length of the string is calculating using the
+length property that is assigned a variable const
+
+The for loop checks if the first and the last successive characters
+of the string are similar.Loop continues till half of the string
 */
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -20,6 +46,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log('Expecing: true');
+  console.log("=>", isPalindrome("abba"))
 }
 
 module.exports = isPalindrome;
